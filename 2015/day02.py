b529f8416c8f46637fedcf0c4f69c4bf129b4982
Archie White <ernie.white.68@gmail.com>
2026-01-03 19:33:01 +0000

    Add Day 02 solutions for Advent of Code 2015

---
+++ 2015/day02.py
@@ -0,0 +1,57 @@
+"""
+Advent of Code - Day 02
+Year: 2015
+"""
+
+
+def part1(data):
+    """
+    Solve part 1 of the puzzle
+    
+    Args:
+        data: Input data as string
+        
+    Returns:
+        Solution to part 1
+    """
+    lines = data.strip().split('\n')
+    # TODO: Implement solution
+    return 0
+
+
+def part2(data):
+    """
+    Solve part 2 of the puzzle
+    
+    Args:
+        data: Input data as string
+        
+    Returns:
+        Solution to part 2
+    """
+    lines = data.strip().split('\n')
+    # TODO: Implement solution
+    return 0
+
+
+def main():
+    """Main function to run the solutions"""
+    # Read input data from file
+    try:
+        with open("input.txt", "r", encoding="utf-8") as f:
+            data = f.read()
+    except FileNotFoundError:
+        print("Error: input.txt not found")
+        print("Please create an input.txt file with your puzzle input")
+        return
+    
+    # Solve and print results
+    result1 = part1(data)
+    print(f"Part 1: {result1}")
+    
+    result2 = part2(data)
+    print(f"Part 2: {result2}")
+
+
+if __name__ == "__main__":
+    main()
