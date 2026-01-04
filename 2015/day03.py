9fece8a0be8d76e11044a7cd4f8ae2eac33c93d0
Archie White <ernie.white.68@gmail.com>
2026-01-04 14:01:52 +0000

    Refactor import statements in day03.py for clarity and organization

--- 2015/day03.py
+++ 2015/day03.py
@@ -2,9 +2,13 @@
 Advent of Code - Day 03
 Year: 2015
 """
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 import time
-from ..utils import utils
+from utils import read_input
 
 
 def part1(data):
